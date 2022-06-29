502513d5faa3a522cf2abfe4a6d520780fb29bbc
Predrag <predrag.lazic95@yahoo.com>
2022-06-29 13:31:40 +0200

    Add files via upload

--- src/assets/View.js
+++ src/assets/View.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const View = () => {
+
+  const [anime, setAnime] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://api.jikan.moe/v4/anime?q=naruto&sfw`)
+      .then(res => {
+        const anime = res.data.data.slice(0, 10);
+        setAnime(anime);
+      })
+  }, [])  
+
   return (
     <div className='view-wrapper'>
-      <h1>Anime Cards</h1>
+      {}
     </div>
   )
 }
